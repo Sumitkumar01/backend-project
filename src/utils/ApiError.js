458d4aa50d5aa13cc27b69b42df194d8757e067d
sumit <sk7627577@gmail.com>
2024-06-24 17:23:49 +0530

    error fix

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -13,7 +13,7 @@ class ApiError extends Error {
         this.errors = errors
 
         if (statck) {
-            this.stack = statck
+            this.stack = stack
         } else {
             Error.captureStackTrace(this, this.constructor)
         }
