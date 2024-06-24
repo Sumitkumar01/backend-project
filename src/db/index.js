5465aed0191a24014e3120c237f880c9e3110ff1
sumit <sk7627577@gmail.com>
2024-06-24 12:56:04 +0530

    content change

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDb = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("mongodb conneection error", error);
+        console.log("mongodb conneection FAILED", error);
         process.exit(1)
     }
 }
