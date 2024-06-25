cfb129a05ca0968bf2a475e4bfe9f7b018f91619
sumit <sk7627577@gmail.com>
2024-06-25 12:23:28 +0530

    adding multer, cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+
+
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Upload an image
+
+const uplodeOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // uplode the file 
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        console.log("file uploaded successfull", response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the uplode operation got failed
+        return null;
+    }
+}
+
