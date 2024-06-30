7ba7772125c67142449618000efbdd244a49a6f6
sumit <sk7627577@gmail.com>
2024-06-30 07:26:42 +0530

    add middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,31 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js"
+import {
+  loginUser,
+  LogOutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login").post(loginUser);
 
+// secured routes
 
+router.route("/logOut").post(verifyJWT, LogOutUser);
 
 export default router;
